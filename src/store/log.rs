afe6d7abf22db99a1a4281d483ce079aeba23373
Cao Jiafeng <funfriendcjf@gmail.com>
2017-04-05 11:01:23 +0800

    impl segment id

--- src/store/log.rs
+++ src/store/log.rs
@@ -1,18 +1,124 @@
+extern crate skiplist;
+use self::skiplist::SkipMap;
+
+extern crate bytebuffer;
+use self::bytebuffer::ByteBuffer;
+
 use std::fs;
 use std::io;
-use std::collections::HashMap;
+use std::io::prelude::*;
+use std::fs::File;
 use std::path::{Path, PathBuf};
 use std::iter::Iterator;
-use std::io::prelude::*;
-use super::log_segment::LogSegment;
-use super::log_segment_name_helper::{ self, get_segment_name };
 
-pub struct Log {
-    data_dir: String,
-    capacity_in_bytes: u64,
-    segment_capacity_in_bytes: u64,
-    segments_by_name: HashMap<String, LogSegment>,
+use super::FILENAME_SEPARATOR;
+
+const SUFFIX: &str = "_log";
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct SegmentId {
+    pos: u64,
+    gen: u64,
 }
+
+fn segment_id_to_filename(id: &SegmentId) -> String {
+    format!("{pos}{sep}{gen}{suffix}",
+            pos = id.pos,
+            sep = FILENAME_SEPARATOR,
+            gen = id.gen,
+            suffix = SUFFIX
+    )
+}
+
+fn next_segment_id_pos(id: &SegmentId) -> SegmentId {
+    SegmentId {
+        pos: id.pos + 1,
+        gen: id.gen,
+    }
+}
+
+
+pub struct LogSegment {
+    // name: String,
+    file: File,
+    capacity_in_bytes: u64,
+    end_offset: u64,
+    start_offset: u64,
+
+}
+
+impl LogSegment {
+    const VERSION: u8 = 0;
+    //    const VERSION_HEADER_SIZE: u32 = 2;
+    //    const CAPACITY_HEADER_SIZE: u32 = 8;
+    //    const CRC_SIZE: u32 = 8;
+
+    pub fn new(file: File, capacity_in_bytes: u64) -> LogSegment {
+        let mut log_segment = LogSegment {
+            file: file,
+            capacity_in_bytes: capacity_in_bytes,
+            start_offset: 0,
+            end_offset: 0,
+        };
+
+        log_segment.write_header().unwrap();
+        log_segment
+    }
+
+    pub fn restore_from(
+        path: &Path,
+        capacity_in_bytes: u64
+    ) -> LogSegment {
+        unimplemented!()
+    }
+
+    // pub fn name(&self) -> &str {
+    //     &self.name
+    // }
+
+    pub fn capacity_in_bytes(&self) -> u64 {
+        self.capacity_in_bytes
+    }
+
+    pub fn remaining_bytes(&self) -> u64 {
+        self.capacity_in_bytes() - self.end_offset
+    }
+
+    fn write_header(&mut self) -> io::Result<()> {
+        let mut header = ByteBuffer::new();
+        header.write_u8(LogSegment::VERSION);
+        header.write_u64(self.capacity_in_bytes);
+        // TODO(caojiafeng): add crc
+
+        let _ = self.write_all(&header.to_bytes())?;
+        self.start_offset = self.end_offset;
+        Ok(())
+    }
+}
+
+impl Drop for LogSegment {
+    fn drop(&mut self) {
+        // TODO(caojiafeng): use `sync_all`?
+        let _ = self.file.flush();
+    }
+}
+
+
+impl Write for LogSegment {
+    fn write(&mut self, data: &[u8]) -> io::Result<usize> {
+        // write until finished
+        let write_size = self.file.write(data)? as u64;
+        self.end_offset += write_size;
+        Ok(write_size as usize)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.file.flush()
+    }
+
+}
+
+// TODO(caojiafeng): impl Read for LogSegment
 
 trait PathExt {
     fn ls_files<F>(&self, preficate: F) -> io::Result<Vec<PathBuf>>
@@ -35,6 +141,14 @@ impl PathExt for Path {
 }
 
 
+pub struct Log {
+    data_dir: String,
+    capacity_in_bytes: u64,
+    segment_capacity_in_bytes: u64,
+    // TODO(caojiafeng): verify that the access to active_segment is O(1)
+    segments_by_name: SkipMap<SegmentId, LogSegment>,
+}
+
 impl Log {
     pub fn new(
         data_dir: &str,
@@ -44,16 +158,16 @@ impl Log {
         fs::create_dir_all(data_dir).unwrap();
 
         // TODO(caojiafeng): handle segment restore
-        let path = Path::new(data_dir);
-        let segment_files = path.ls_files(|x| x.ends_with("_log")).unwrap();
+        // let path = Path::new(data_dir);
+        // let segment_files = path.ls_files(|x| x.ends_with("_log")).unwrap();
 
-        let mut segments_by_name = HashMap::<String, LogSegment>::new();
+        let mut segments_by_name = SkipMap::<SegmentId, LogSegment>::new();
 
         // create first segment
-        let name = get_segment_name(0, 0);
-        let filename = log_segment_name_helper::name_to_filename(&name);
+        let name = SegmentId { pos: 0, gen: 0 };
+        let filename = segment_id_to_filename(&name);
         let segment_file = fs::File::create(&filename).unwrap();
-        let segment = LogSegment::new(name.clone(), segment_file, segment_capacity_in_bytes);
+        let segment = LogSegment::new(segment_file, segment_capacity_in_bytes);
         segments_by_name.insert(name, segment);
             // Self::load_segments(segment_files, segment_capacity_in_bytes);
 
@@ -66,8 +180,12 @@ impl Log {
         log
     }
 
-    pub fn active_segment(&self) -> &mut LogSegment {
-        unimplemented!()
+    fn active_segment_mut(&mut self) -> (&SegmentId, &mut LogSegment)  {
+        self.segments_by_name.back_mut().unwrap()
+    }
+
+    fn active_segment(&self) -> (&SegmentId, &LogSegment) {
+        self.segments_by_name.back().unwrap()
     }
 
     fn load_segments(paths: Vec<PathBuf>, segment_capacity_in_bytes: u64) {
@@ -77,14 +195,14 @@ impl Log {
 
 impl Log {
     fn should_rollover(&self, write_size: u64) -> bool {
-        self.active_segment().remaining_bytes() < write_size
+        self.active_segment().1.remaining_bytes() < write_size
     }
 
     fn rollover(&mut self) -> io::Result<()> {
-        let name = log_segment_name_helper::next_pos_name(self.active_segment().name());
-        let filename = log_segment_name_helper::name_to_filename(&name);
+        let name = next_segment_id_pos(self.active_segment().0);
+        let filename = segment_id_to_filename(&name);
         let segment_file = fs::File::create(&filename).unwrap();
-        let segment = LogSegment::new(name.clone(), segment_file, self.segment_capacity_in_bytes);
+        let segment = LogSegment::new(segment_file, self.segment_capacity_in_bytes);
         self.segments_by_name.insert(name, segment);
         Ok(())
     }
@@ -95,20 +213,17 @@ impl Write for Log {
         if self.should_rollover(data.len() as u64) {
             self.rollover().unwrap();
         }
-        self.active_segment().write(data)
+        self.active_segment_mut().1.write(data)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        unimplemented!()
+        self.active_segment_mut().1.flush()
     }
 
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use std::path::Path;
-
     #[test]
     fn test_read_dir() {}
 }
--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,9 +1,7 @@
-pub use self::log_segment::LogSegment;
+pub use self::log::LogSegment;
 pub use self::log::Log;
 
 
-mod log_segment;
-mod log_segment_name_helper;
 mod log;
 
 const FILENAME_SEPARATOR: &str = "_";
